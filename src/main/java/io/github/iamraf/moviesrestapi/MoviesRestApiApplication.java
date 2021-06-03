b04295fa5d413e8c2247ca1a1ed56b7fd0c0c574
Raf <iamraf@protonmail.com>
2021-06-03 08:11:48 +0300

    Initialize project

---
+++ src/main/java/io/github/iamraf/moviesrestapi/MoviesRestApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.iamraf.moviesrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/iamraf/moviesrestapi/MoviesRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.iamraf.moviesrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
